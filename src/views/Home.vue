4682b42e7142929f38a0b8f075e2852bf558af3e
Jonathan Brown <jbrown@mix-blockchain.org>
2020-07-22 11:59:46 +0700

    Add messages.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <v-container>
-    <v-row align="center" style="margin: 40px 0;">
+    <v-row style="margin: 40px 0;">
       <v-col>
-        <v-img src="images/acuity-logo.svg" alt="Acuity logo" contain height="200" id="acuity-animation"></v-img>
+        <v-img src="images/acuity-logo.svg" alt="Acuity logo" contain height="200" width="200" id="acuity-animation" style="margin-left: auto; margin-right: auto;"></v-img>
       </v-col>
     </v-row>
     <v-row style="margin: 40px 0;">
       <v-col>
         <div style="color: #3cc3db; font-family: Raleway; text-align: center; font-size: 24px;">
-          <router-link to="/regenesis">Regenesis</router-link> in: {{ days }} {{ $tc('days', days) }}, {{ hours }} {{ $tc('hours', hours) }}, {{ minutes }} {{ $tc('minutes', minutes) }}, {{ seconds }} {{ $tc('seconds', seconds) }}
+          {{ message }}<span class="blink-me">|</span><br /> <br />
+          <router-link to="/regenesis">Regenesis</router-link> in: {{ days }} {{ $tc('days', days) }}, {{ hours }} {{ $tc('hours', hours) }}, {{ minutes }} {{ $tc('minutes', minutes) }}, {{ seconds }} {{ $tc('seconds', seconds) }}<br />
+          MIX => ACUITY
         </div>
       </v-col>
     </v-row>
@@ -29,6 +31,17 @@
 <script lang="ts">
   import Vue from 'vue'
 
+  function shuffle(input: any[]) {
+  	for (let i = input.length - 1; i >= 0; i--) {
+  		let randomIndex = Math.floor(Math.random() * (i + 1))
+  		let itemAtIndex = input[randomIndex]
+  		input[randomIndex] = input[i]
+  		input[i] = itemAtIndex
+  	}
+
+  	return input
+  }
+
   export default Vue.extend({
     name: 'Home',
 
@@ -36,24 +49,32 @@
     },
 
     data: () => ({
+      message: '',
       messages: [
-        'The world we need.',
-        'Neutrality without coersion.',
-        'Hardfork humanity.',
-        'Knowledge discovery.',
-        'Unstoppable publication.',
-        'Collaborative investigation.',
-        'The medium is the message.',
-        'Federated blockchains.',
-        'Your own space.',
-        'Level playing field.',
-        'Eradicate corruption?',
-        'Discover our history.',
-        'Ungameable metrics.',
-        'Regenesis.',
-        '1) They came for the journalists. 2) ?',
-        'Combat logical fallacies.'
+        'The world we need. ',
+        'Neutrality without coersion. ',
+        'Hardfork humanity. ',
+        'Knowledge discovery. ',
+        'Unstoppable publication. ',
+        'Collaborative investigation. ',
+        'The medium is the message. ',
+        'Federated blockchains. ',
+        'Your own space. ',
+        'Eradicate corruption? ',
+        'Discover our history. ',
+        'Ungameable metrics. ',
+        '1) They came for the journalists. 2) ? ',
+        'Combat logical fallacies. ',
+        'All wars are bankers\' wars. ',
+        'Cui bono? ',
+        'Follow the money. ',
+        'Reclaim the Web. ',
+        'Break the wheel. ',
       ],
+      row: 0,
+      column: 0,
+      sleep: 20,
+      delete: false,
       days: 0,
       hours: 0,
       minutes: 0,
@@ -61,11 +82,39 @@
     }),
 
     created() {
+      setInterval(this.updateMessage, 100)
       setInterval(this.updateTimeLeft, 1000)
       this.updateTimeLeft()
+
+      this.messages = shuffle(this.messages)
     },
 
     methods: {
+      updateMessage() {
+        if (this.sleep > 0) {
+          this.sleep--
+          return
+        }
+        if (this.delete) {
+          this.message = this.message.slice(0, -10)
+          if (this.message.length == 0) {
+            this.row++
+            if (this.row == this.messages.length) {
+              this.row = 0
+            }
+            this.column = 0
+            this.delete = false
+          }
+        }
+        else {
+          this.message += this.messages[this.row][this.column]
+          this.column++
+          if (this.column == this.messages[this.row].length) {
+            this.delete = true
+            this.sleep = 40
+          }
+        }
+      },
       updateTimeLeft() {
         let msLeft = 1598918400000 - Date.now()
         this.days = Math.floor(msLeft / 86400000)
@@ -97,6 +146,16 @@
 @keyframes rotate {
     from {transform: rotate(0deg);}
     to {transform: rotate(360deg);}
+}
+
+.blink-me {
+  animation: blinker 1s linear infinite;
+}
+
+@keyframes blinker {
+  50% {
+    opacity: 0;
+  }
 }
 
 </style>
