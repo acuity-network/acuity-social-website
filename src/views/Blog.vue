661a7cb8fd72e253e9ddc68527f6563b584d523b
Jonathan Brown <jbrown@acuity.social>
2020-11-26 15:41:17 +0700

    Add hard spoon blog post.

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -4,6 +4,25 @@
       <v-col cols="12" md="10">
         <v-card elevation="10" outlined>
           <v-card-text>
+            <div class="text-h5 mb-1">Acuity hard spooned!</div>
+            <div class="subtitle-2 mb-4">26th November 2020 by Jonathan Brown</div>
+            <div class="body-2 mb-4">
+              <p>Due to the cessation of block production of the proof-of-authority nodes (see previous blog post), it was necessary for Acuity to <a target="_blank" href="https://wiki.polkadot.network/docs/ru/glossary#hard-spoon">hard spoon</a>. A snapshot of blockchain state was taken at block 1049095 (the last finalized block) and a new genesis block was created.</p>
+              <p>Two modifications to the state were made:
+                <ul>
+                  <li>runtime upgrade - Built with the latest version of <a target="_blank" href="https://github.com/acuity-social/acuity-substrate">acuity-substrate</a>, this may fix the problem of new validators being unable to join the network  (and prevent cessation of block production from re-occuring).</li>
+                  <li>sudo pallet - I have given myself admin access to the blockchain. This means I can fix any problems immediately and perform network upgrades without going through the governance mechanism. This feature will be removed once Acuity is more mature.</li>
+                </ul>
+              </p>
+              <p>The blockchain has restarted from a fresh block 0. Let me know if anything is not as expected. The emphasis now is to ensure ACU can be traded freely. This will involve integrating with centralized exchanges, integrating with atomic swap decentralized exchanges (or building our own), and creating Wrapped ACU as an Ethereum ERC20 token.</p>
+              <p>Really sorry for this extended outage - let's march forward with creating the most fully decentralized social media platform in existence!</p>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="10">
+        <v-card elevation="10" outlined>
+          <v-card-text>
             <div class="text-h5 mb-1">Acuity Social November 2020 Project Update</div>
             <div class="subtitle-2 mb-4">19th November 2020 by Jonathan Brown</div>
             <div class="body-2 mb-4">
