2f15fca220a92f8f18347e89d8c29cfffbb6636e
Jonathan Brown <jbrown@acuity.social>
2021-08-27 05:46:19 +0700

    Fix Acuity types.

--- src/lib/AcuityClient.ts
+++ src/lib/AcuityClient.ts
@@ -7,7 +7,18 @@ export default class MixClient {
 
     let acuityEndpoint = (process.env.NODE_ENV == 'development') ? 'ws://127.0.0.1:9946' : 'wss://acuity.social:9961';
     let wsProvider = new WsProvider(acuityEndpoint);
-    await ApiPromise.create({ provider: wsProvider }).then(async api => {
+    await ApiPromise.create({
+      provider: wsProvider,
+      types: {
+        AcuityOrderId: '[u8; 16]',
+        AcuityAssetId: '[u8; 16]',
+        AcuityForeignAddress: '[u8; 32]',
+        AcuityHashedSecret: '[u8; 32]',
+        AcuitySecret: '[u8; 32]',
+        Timestamp: 'u64',
+        EthereumAddress: '[u8; 20]',
+      }
+    }).then(async api => {
       this.api = api;
       await this.api.isReady
     });
