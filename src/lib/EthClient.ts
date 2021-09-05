cc3830f0801d000294097e8dfbfe6030069f3285
Jonathan Brown <jbrown@acuity.social>
2021-09-05 11:19:13 +0700

    Add Ethereum smart contract ABIs.

--- src/lib/EthClient.ts
+++ src/lib/EthClient.ts
@@ -4,6 +4,8 @@ import Web3 from 'web3'
 export default class MixClient {
 	web3: any
   formatWei: any
+  atomicSwapSell: any;
+  atomicSwapBuy: any;
 
 	async init(vue: any) {
     // This function detects most providers injected at window.ethereum
@@ -11,10 +13,11 @@ export default class MixClient {
 
     if (provider) {
       this.web3 = new Web3(provider);
-  		console.log(await this.web3.eth.getProtocolVersion());
   		this.web3.eth.defaultBlock = 'pending';
   		this.web3.eth.transactionConfirmationBlocks = 1;
       this.formatWei = (wei: string) => Number(this.web3.utils.fromWei(this.web3.utils.toBN(wei))).toLocaleString();
+      this.atomicSwapSell = new this.web3.eth.Contract(require('./contracts/AcuityAtomicSwapSell.abi.json'), '0xd05647dd9D7B17aBEBa953fbF2dc8D8e87c19cb3');
+  		this.atomicSwapBuy = new this.web3.eth.Contract(require('./contracts/AcuityAtomicSwapBuy.abi.json'), '0x744Ac7bbcFDDA8fdb41cF55c020d62f2109887A5');
     } else {
       console.log('Please install MetaMask!');
     }
