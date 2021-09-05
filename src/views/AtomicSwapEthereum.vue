1d992bd3240842b46c03815395695712c107e4b4
Jonathan Brown <jbrown@acuity.social>
2021-09-05 11:20:00 +0700

    Submit Ethereum addToSellOrder tx.

--- src/views/AtomicSwapEthereum.vue
+++ src/views/AtomicSwapEthereum.vue
@@ -2,15 +2,23 @@
   <v-container>
     <v-row>
       <v-col cols="12" md="10">
-        <div class="text-h5 mb-1">Create Ethereum sell order</div>
-        <v-select v-model="sell_asset" :items="sell_assets" label="Sell Asset"></v-select>
-        <v-select v-model="sell_address" :items="sell_addresses" label="Sell Account"></v-select>
-        <v-text-field v-model="sell_value" label="Sell Value"></v-text-field>
-        <v-select v-model="buy_blockchain" :items="buy_blockchains" label="Buy Blockchain"></v-select>
-        <v-select v-model="buy_asset" :items="buy_assets" label="Buy Asset"></v-select>
-        <v-select v-model="buy_address" :items="buy_addresses" label="Buy Account"></v-select>
-        <v-text-field v-model="price" label="Price"></v-text-field>
-        <v-btn @click="addSellOrder">Create Sell Order</v-btn>
+        <div class="text-h5 mb-1">Sell ETH for ACU</div>
+        <div class="body-2 mb-4">Acuity accounts are managed by the <a target="_blank" href="https://polkadot.js.org/extension/">Polkadot</a> browser extension.</div>
+        <div class="body-2 mb-4">Ethereum accounts are managed by the <a target="_blank" href="https://metamask.io/">Metamask</a> browser extension.</div>
+        <v-form v-model="valid">
+        <!--
+          <v-select v-model="sell_asset" :items="sell_assets" label="Sell Asset"></v-select>
+        -->
+          <v-select v-model="sell_address" :items="sell_addresses" label="Sell Account"></v-select>
+          <v-text-field v-model="sell_value" label="Sell Value"></v-text-field>
+        <!--
+          <v-select v-model="buy_blockchain" :items="buy_blockchains" label="Buy Blockchain"></v-select>
+          <v-select v-model="buy_asset" :items="buy_assets" label="Buy Asset"></v-select>
+        -->
+          <v-select v-model="buy_address" :items="buy_addresses" label="Buy Account"></v-select>
+          <v-text-field v-model="price" label="Price"></v-text-field>
+          <v-btn @click="addSellOrder">Create Sell Order</v-btn>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
@@ -61,14 +69,10 @@
 
     methods: {
       async addSellOrder(event: any) {
-/*        const SENDER = this.$activeAccount.get();
-        const injector = await web3FromAddress(SENDER);
-        this.$acuityClient.api.tx.atomicSwap
-          .addToOrder('0x', this.price, this.foreign_address, this.value)
-          .signAndSend(SENDER, { signer: injector.signer }, (status: any) => {
-            console.log(status)
-          });
-*/
+        let buy_address = this.$ethClient.web3.utils.padLeft(this.buy_address, 64);
+        let value = this.$ethClient.web3.utils.toWei(this.sell_value);
+        let price = this.$ethClient.web3.utils.toWei(this.price);
+        this.$ethClient.atomicSwapSell.methods.addToOrder("0x8888888888888888888888888888888888888888888888888888888888888888", "0x8888888888888888888888888888888888888888888888888888888888888888").send({from: this.sell_address, value: value});
       }
     }
   })
