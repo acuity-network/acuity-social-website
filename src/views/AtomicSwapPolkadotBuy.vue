c1ab0ad136c3f9e193a3c1eb3306257af711e520
Jonathan Brown <jbrown@acuity.social>
2021-10-01 15:00:21 +0700

    Format buy locks.

--- src/views/AtomicSwapPolkadotBuy.vue
+++ src/views/AtomicSwapPolkadotBuy.vue
@@ -1,33 +1,45 @@
 <template>
   <v-container>
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">
-              Buyer
-            </th>
-            <th class="text-left">
-              Value (ETH)
-            </th>
-            <th class="text-left">
-              Timeout
-            </th>
-            <th class="text-left">
-              Operations
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="buyLock in buyLocks" :key="buyLock.hashed_secret">
-            <td>{{ buyLock.buyer }}</td>
-            <td>{{ buyLock.value }}</td>
-            <td>{{ buyLock.timeout }}</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <v-row>
+      <v-col cols="12" md="10">
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Buyer
+                </th>
+                <th class="text-left">
+                  Buy Lock
+                </th>
+                <th class="text-left">
+                  Value (ETH)
+                </th>
+                <th class="text-left">
+                  Sell Lock
+                </th>
+                <th class="text-left">
+                  Value (ACU)
+                </th>
+                <th class="text-left">
+                  Operations
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="buyLock in buyLocks" :key="buyLock.hashed_secret">
+                <td>{{ buyLock.buyer }}</td>
+                <td>{{ buyLock.timeout }}</td>
+                <td>{{ buyLock.value }}</td>
+                <td></td>
+                <td>{{ buyLock.valueAcu }}</td>
+                <td><v-btn icon @click=""><v-icon small>mdi-atom-variant</v-icon></v-btn></td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
     <v-row>
       <v-col cols="12" md="10">
         <div class="text-h5 mb-1">Buy ACU</div>
@@ -70,7 +82,20 @@
     computed: {
       buyLocks(): {}[] {
         if (this.orderId in this.$store.state.ordersAcu) {
-          return this.$store.state.ordersAcu[this.orderId].buy_locks;
+
+          let buy_locks = [];
+
+          for (let buy_lock of this.$store.state.ordersAcu[this.orderId].buy_locks) {
+            buy_locks.push({
+              buyer: buy_lock.buyer,
+              timeout: new Date(buy_lock.timeout * 1000).toLocaleString(),
+              value: this.$ethClient.web3.utils.fromWei(buy_lock.value.toString()),
+              valueAcu: this.$ethClient.web3.utils.fromWei((BigInt(this.$ethClient.web3.utils.toWei(buy_lock.value.toString())) / BigInt(this.priceWei)).toString()),
+//              valueAcu: parseFloat(buy_lock.value.toString()) / parseFloat(this.priceWei),
+            })
+          }
+
+          return buy_locks;
         }
         else {
           return [];
@@ -130,7 +155,7 @@
 
     methods: {
       async buy() {
-        let value = this.$ethClient.web3.utils.fromWei((BigInt(this.$ethClient.web3.utils.toWei(this.value)) * BigInt(this.priceWei)).toString());
+        let value = this.$ethClient.web3.utils.fromWei((BigInt(this.$ethClient.web3.utils.toWei(this.value)) * BigInt(this.priceWei)).toString()).split('.')[0];
         let hashedSecret = this.$ethClient.web3.utils.randomHex(32);
         let assetIdOrderId = "0x88888888888888888888888888888888" + this.orderId;
         let timeout = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 3;
