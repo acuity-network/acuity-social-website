0a36cefcfd11898ffc9d52cc93c9ca61064d5a5e
Jonathan Brown <jbrown@acuity.social>
2021-12-20 12:32:29 +0700

    Add blog post.

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -9,6 +9,23 @@
       <v-col cols="12" md="10">
         <v-card elevation="10" outlined>
           <v-card-text>
+            <div class="text-h5 mb-1">Hard Spoon Completed</div>
+            <div class="subtitle-2 mb-4">14th December 2021 by Jonathan Brown</div>
+            <div class="body-2 mb-4">
+              <p>The <a target="_blank" href="https://wiki.polkadot.network/docs/glossary#hard-spoon">hard spoon</a> announced in the previous blog post has now occurred. At block #3276706 a snapshot was recorded of all ACU balances and unclaimed MIX => ACU.</p>
+              <p>A new genesis block was created and the new blockchain started to facilitate the upcoming launch of the <router-link to="/atomic-swap">Acuity DEX</router-link>. This was the third (and hopefully final) hard spoon that has been performed on the Acuity blockchain.</p>
+              <p>If you are running a full node, you will need to update to the <a target="_blank" href="https://github.com/acuity-social/acuity-substrate/releases">latest release</a> of the Acuity software and sync from scratch.</p>
+              <p>The Polkadot <a target="_blank" href="https://polkadot.acuity.social/">web interface</a> for Acuity has been updated to the latest version and is now connected to the new blockchain. You might need to give it a hard refresh.</p>
+              <p>All balances have been brought over to the new blockchain. Our exchange partner <a target="_blank" href="https://app.stex.com/en/trading/pair/ETH/ACU/1D">STEX</a> has already updated their platform.</p>
+              <p>If you were staking, either as a nominator or a validator, you will need to <a target="_blank" href="https://www.youtube.com/watch?v=M8Cju6M06MQ">set this up</a> again.</p>
+              <p>We can now move forward with launching the Acuity Atomic Swap Exchange that will enable full decentralized trade between many different blockchains.</p>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="10">
+        <v-card elevation="10" outlined>
+          <v-card-text>
             <div class="text-h5 mb-1">Acuity Atomic Swap DEX - the future of interchain trading</div>
             <div class="subtitle-2 mb-4">4th December 2021 by Jonathan Brown</div>
             <div class="body-2 mb-4">
