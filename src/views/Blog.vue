e9e117d30aeedee3d6c201c9c89fc611ab6a176e
Jonathan Brown <jbrown@acuity.social>
2022-06-02 20:55:37 +0700

    New blog post.

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -9,6 +9,22 @@
       <v-col cols="12" md="10">
         <v-card elevation="10" outlined>
           <v-card-text>
+            <div class="text-h5 mb-1">ACU is becoming a DeFi-only coin</div>
+            <div class="subtitle-2 mb-4">2nd June 2022 by Jonathan Brown</div>
+            <div class="body-2 mb-4">
+              <p>As detailed in the earlier blog post below "Acuity Atomic Swap DEX - the future of interchain trading", centralized exchanges are not only a risk to those storing their coins on such exchanges. CEXs are actually a systemic risk to the trading price of coins that are traded on them. It is a security vulnerability for a project to have its coin listed on a centralized exchange.</p>
+              <p>DeFi Maximilism is a concept whose day will come. The incompatibility of CEXs with the blockchain narrative will become obvious. It will seem very strange that cryptocurrencies were ever listed on exchanges that have the authority to manipulate supply as they please.</p>
+              <p>The realization of this is what prompted Acuity to develop its own <router-link to="/atomic-swap">Atomic Swap Decentralized Exchange</router-link>. As can be seem from this blog we have spent the last one and a half years researching the best approach to implementing a cross-chain fully autonomous exchange. This process is now drawing to a close.</p>
+              <p>The Acuity DEX is nearing launch and will enable trading of all cross-chain pairs of ACU, and all coins and ERC20 tokens on all major EVM blockchains including Ethereum, Optimism, Polygon, Arbitrum, Moonbeam, smartBCH, Avalanche, etc.</p>
+              <p>In order to lead by example, trading of ACU on its only remaining CEX, STEX, has ceased. ACU can continue to be withdrawn from STEX until 31st June 2022.</p>
+              <p>ACU can currently be traded OTC and the DEX will be deployed soon.</p>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="10">
+        <v-card elevation="10" outlined>
+          <v-card-text>
             <div class="text-h5 mb-1">Social Trading</div>
             <div class="subtitle-2 mb-4">18th April 2022 by Jonathan Brown</div>
             <div class="body-2 mb-4">
